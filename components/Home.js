772290bb6edd635d302b4f625e19be3d856d5938
Ben <tancheanyong@gmail.com>
2022-02-19 23:00:16 +0800

    added async storage

--- components/Home.js
+++ components/Home.js
@@ -1,37 +1,78 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Button } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import LinearGradient from 'react-native-linear-gradient'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const Home = () => {
+  const [display,setDisplay]=useState({});
+  
+  useEffect(()=>{
+    async function fetchData(){
+      try{
+        let keys=await AsyncStorage.getAllKeys();
+        if(keys.length){
+          let obj=await AsyncStorage.getItem(keys[Math.floor(Math.random() * (keys.length))])
+          console.log(obj);
+          setDisplay(JSON.parse(obj));
+        }
+      }catch(e){
+        console.log(e)
+      }
+    }
+    fetchData();
+  },[])
+
+  const deleteAll=()=>{
+    AsyncStorage.getAllKeys()
+        .then(keys => AsyncStorage.multiRemove(keys))
+        .then(() => alert('success'))
+  }
   return (
     <View style={styles.body}>
       <LinearGradient colors={['#4c669f', '#3b5998', '#192f6a']} style={styles.card}>
-        <Text>Title</Text>
-        <View>
-          <Text>Mood</Text>
-          <Text>27 March 2022</Text>
+        <Text style={styles.cardTitle}>{display.title}</Text>
+        <View style={styles.cardInfo}>
+          <Text style={styles.cardInfoText}>{display.mood}</Text>
+          <Text style={styles.cardInfoText}>{display.key}</Text>
         </View>
-        <Text>
-          “If you are distressed by anything external, the pain is not due to the thing itself, but to your estimate of it; and this you have the power to revoke at any moment.”
-          ― Marcus Aurelius, Meditations
+        <Text style={styles.cardText}>
+          {display.text}
         </Text>
       </LinearGradient>
+      <Button title='delete all thoughts' onPress={()=>deleteAll()} />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  body:{
-    backgroundColor:'gray',
-    flex:1
+  body: {
+    backgroundColor: 'gray',
+    flex: 1
   },
-  card:{
-    marginVertical:30,
-    marginHorizontal:50,
-    padding:20,
+  card: {
+    marginVertical: 30,
+    marginHorizontal: 50,
+    padding: 20,
     borderColor: 'white',
-    borderWidth:1,
-    borderRadius:10
+    borderWidth: 1,
+    borderRadius: 10
+  },
+  cardTitle: {
+    color: 'white',
+    fontSize: 25,
+    fontWeight: 'bold'
+  },
+  cardInfo: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  cardInfoText: {
+    color: 'lightgray',
+  },
+  cardText: {
+    color: 'white',
+    fontSize: 20,
+    marginVertical: 20
   }
 })
 
--- components/Post.js
+++ components/Post.js
@@ -1,12 +1,95 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput,StyleSheet, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { Picker } from '@react-native-picker/picker'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import moment from 'moment'
 
 const Post = () => {
+  const [title,setTitle]=useState('');
+  const [text,setText]=useState('');
+  const [mood,setMood]=useState('Happy');
+
+  const postHandler=async()=>{
+    try{
+      let dateTime=moment().format('DD MM YYYY, kk:mm:ss');
+      let obj={
+        'key':dateTime,
+        'title':title,
+        'text':text,
+        'mood':mood
+      }
+      await AsyncStorage.setItem(obj.key,JSON.stringify(obj))
+      console.log('success');
+    }catch(e){
+      console.log(e);
+    }
+  }
+
   return (
-    <View>
-      <Text>Post</Text>
+    <View style={styles.body}>
+      <Text style={styles.pageTitle}>What's on your mind?</Text>
+      <TextInput style={styles.inputTitle} multiline={true} onChangeText={(text)=>setTitle(text)} value={title} placeholder='Title' />
+      <TextInput style={styles.input} multiline={true} textAlignVertical='top' onChangeText={(text)=>setText(text)} value={text} />
+      <Picker
+        selectedValue={mood}
+        onValueChange={(itemValue)=>setMood(itemValue)}>
+        <Picker.Item label="Happy" value="Happy" />
+        <Picker.Item label="Sad" value="Sad" />
+      </Picker>
+      <View style={styles.btnContainer}>
+        <TouchableOpacity style={styles.saveBtn} onPress={()=>postHandler()}>
+          <Text style={styles.btnText}>Save this thought</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  body:{
+    backgroundColor:'gray',
+    flex:1,
+  },
+  pageTitle:{
+    fontSize:25,
+    textAlign:'center',
+    color:'white',
+    margin:20,
+    fontWeight:'bold'
+  },
+  inputTitle:{
+    fontSize:17,
+    borderWidth:2,
+    borderColor:'blue',
+    backgroundColor:'lightgray',
+    marginHorizontal:20,
+    borderRadius:10,
+    marginBottom:5,
+    padding:10
+  },
+  input:{
+    borderWidth:2,
+    borderColor:'blue',
+    backgroundColor:'lightgray',
+    marginHorizontal:20,
+    borderRadius:10,
+    flex:1,
+    padding:10
+  },
+  btnContainer:{
+    alignItems:'center'
+  },
+  saveBtn:{
+    backgroundColor:'lightblue',
+    padding:10,
+    width:200,
+    borderRadius:10,
+  },
+  btnText:{
+    textAlign:'center',
+    fontSize:20
+  }
+
+})
 
 export default Post
